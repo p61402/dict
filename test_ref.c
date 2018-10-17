7958ba319b71311799fdf1496941200c7b05e29d
QOO <p61402@gmail.com>
2018-10-17 20:28:41 +0800

    Modify test_ref.c to avoid segmentation fault

--- test_ref.c
+++ test_ref.c
@@ -70,7 +70,7 @@ int main(int argc, char **argv)
 
     if (argc == 2 && strcmp(argv[1], "--bench") == 0) {
         int stat = bench_test(root, BENCH_TEST_FILE, LMAX);
-        tst_free_all(root);
+        tst_free(root);
         return stat;
     }
 
